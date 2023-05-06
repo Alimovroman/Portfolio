995ce50cce5cdcd006b4910e7a94cec1ceb9d150
Roman <Alimov.ramon@gmail.com>
2023-05-07 00:30:53 +0300

    fixed particles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,67 +5,7 @@ import Particles from "react-tsparticles";
 import type {Container, Engine} from "tsparticles-engine";
 import {loadFull} from "tsparticles";
 
-const particlesOpt = {
-
-    interactivity: {
-        modes: {
-            bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40
-            },
-            push: {
-                quantity: 4
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            }
-        }
-    },
-    particles: {
-        color: {
-            value: "#ffffff"
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1
-        },
-        collisions: {
-            enable: true
-        },
-        move: {
-            // direction: "none",
-            // enable: true,
-            // outMode: "bounce",
-            // random: false,
-            // speed: 6,
-            // straight: false
-        },
-        number: {
-            density: {
-                enable: true,
-                value_area: 800
-            },
-            value: 80
-        },
-        opacity: {
-            value: 0.5
-        },
-        shape: {
-            type: "circle"
-        },
-        size: {
-            random: true,
-            value: 5
-        }
-    }
-
-}
+const particlesOpt = {}
 
 const Main = () => {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -85,7 +25,68 @@ const Main = () => {
         <div className={style.mainBlock}>
             <Particles id={"tsparticles"}
                        className={style.particles}
-                       options={particlesOpt}
+                       options={
+                           {
+                               // fpsLimit: 120,
+                               // interactivity: {
+                               //
+                               //     modes: {
+                               //         push: {
+                               //             quantity: 4,
+                               //         },
+                               //         repulse: {
+                               //             distance: 200,
+                               //             duration: 0.4,
+                               //         },
+                               //     },
+                               // },
+                               fullScreen: {
+                                   enable: false,
+                                   },
+                               particles: {
+                                   color: {
+                                       value: "#ffffff",
+                                   },
+                                   links: {
+                                       color: "#ffffff",
+                                       distance: 150,
+                                       enable: true,
+                                       opacity: 0.5,
+                                       width: 1,
+                                   },
+                                   collisions: {
+                                       enable: true,
+                                   },
+                                   move: {
+                                       // directions: "none",
+                                       enable: true,
+                                       outModes: {
+                                           default: "bounce",
+                                       },
+                                       random: false,
+                                       speed: 4,
+                                       straight: false,
+                                   },
+                                   number: {
+                                       density: {
+                                           enable: true,
+                                           area: 800,
+                                       },
+                                       value: 80,
+                                   },
+                                   opacity: {
+                                       value: 0.5,
+                                   },
+                                   shape: {
+                                       type: "circle",
+                                   },
+                                   size: {
+                                       value: {min: 1, max: 5},
+                                   },
+                               },
+                               detectRetina: true
+                           }
+                       }
                        init={particlesInit}
                        loaded={particlesLoaded}
             />
