60915bfe64643029d75ee925ed7824bcc53a2dad
Roman <Alimov.ramon@gmail.com>
2023-05-23 00:51:29 +0300

    add form feedback

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -6,11 +6,17 @@ type Props = {
     style?: {
         background: string
     }
+    callBack?: () => void
 }
 const Button: FC<Props> = (props) => {
     return (
         <div>
-            <button className={style.button} style={props.style} type={"submit"}>{props.title}</button>
+            <button className={style.button}
+                    style={props.style}
+                    type={"submit"}
+                    onClick={props.callBack}
+            >{props.title}
+            </button>
         </div>
     );
 };
--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, {FormEvent, FormEventHandler, useRef, useState} from 'react';
 import style from './Contacts.module.scss';
 import Title from "../../common/Title/Title";
 import Button from "../../common/Button/Button";
+import {useForm} from "react-hook-form";
+import emailjs from '@emailjs/browser';
+
+
+type FormData = {
+    name: string
+    email: string
+    message: string
+}
 
 const Contacts = () => {
+    // const [name,setName] =useState('')
+    // const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+    const form = useRef<HTMLFormElement | null>(null);
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (form.current) {
+            emailjs.sendForm('service_5nyhgh4', 'template_oovnf1j', form.current, 'AdSf-sukNqzIlMfXC')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                })
+        }
+        e.currentTarget.reset();
+    };
+
     return (
         <div id={'contacts'} className={style.contactsBlock}>
             <div className={style.contactsContainer}>
-                <Title title={'Contacts'} />
-                <form action={''} className={style.form}>
+                <Title title={'Contacts'}/>
+                <form ref={form} action={''} className={style.form} onSubmit={sendEmail}>
                     <div className={style.inputWrapper}>
-                        <input type={'text'} placeholder={'input'}/>
+                        <input type={'text'} placeholder={'Your name'} name={"name"} required/>
                     </div>
                     <div className={style.inputWrapper}>
-                        <input type={'text'} placeholder={'input'}/>
+                        <input type={'text'} placeholder={'Your Email'} name={"email"} required/>
                     </div>
                     <div className={style.textareaWrapper}>
-                        <textarea placeholder={'Enter the text'} />
+                        <textarea placeholder={'Please write what you want'} name={"message"}/>
                     </div>
                     <div>
-                        <Button title={'Send'} />
+                        <Button title={'Send'}/>
                     </div>
-
                 </form>
 
             </div>
         </div>
     );
 };
+
 
 export default Contacts;
